041586dcd260119e31be5283c12dcbd1107f0818
Ryan <chanr4172@gmail.com>
2025-07-27 12:04:39 +1000

    These are the first files needed for this project

---
+++ main.py
@@ -0,0 +1,18 @@
+
+print("Welcome to the Expense tracker")
+
+"""
+    Add expenses (amount, category: food / transport / entertainment …).  
+    View all expenses chronologically.  
+    Calculate total spending. 
+
+"""
+
+def menu():
+    print("1. Add expenses")
+    print("2. View all expenses")
+    print("3. Calculate total spending")
+    menu_option  = input("Choose which category you want to view: ")
+    
+menu()
+
